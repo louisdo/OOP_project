2ad404da411d6b42dea432dccc9e50bf973b8ebe
Mai Quốc Việt <64018120+Viet-mq@users.noreply.github.com>
2020-06-02 20:23:00 +0700

    Update design.py

--- design.py
+++ design.py
@@ -1,44 +1,44 @@
+import tkinter.messagebox as msbox
 from tkinter import *
 from tkinter.ttk import Frame, Label, Entry
-import tkinter.messagebox as msbox
 
 
 class Design(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.parent = parent
-        self.inputBar()
-        self.functionButton()
+        self.input_bar()
+        self.function_button()
 
-    def inputBar(self):
+    def input_bar(self):
         self.parent.title("Result")
         self.pack(fill=BOTH, expand=True)
 
         frame = Frame(self)
-        frame.pack(fill=BOTH, expand= True)
+        frame.pack(fill=BOTH, expand=True)
 
         lable = Label(frame, text="Input", width=6)
-        lable.pack(side=LEFT, anchor= N, padx=5, pady=5)
+        lable.pack(side=LEFT, anchor=N, padx=5, pady=5)
 
         entry = Entry(frame)
-        entry.pack(fill=BOTH, padx= 5, pady= 5, expand=True)
+        entry.pack(fill=BOTH, padx=5, pady=5, expand=True)
 
-    def functionButton(self):
-        frameButton = Frame(self, relief=RAISED, borderwidth=1)
-        frameButton.pack(fill=BOTH, expand=True)
+    def function_button(self):
+        frame_button = Frame(self, relief=RAISED, borderwidth=1)
+        frame_button.pack(fill=BOTH, expand=True)
 
         self.pack(fill=BOTH, expand=True)
-        closeButton = Button(self, text="Close", command=self.quit)
-        closeButton.pack(side=RIGHT, padx=5, pady=5)
+        close_button = Button(self, text="Close", command=self.quit)
+        close_button.pack(side=RIGHT, padx=5, pady=5)
 
-        okButton = Button(self, text="OK", command=self.showResult)
-        okButton.pack(side=RIGHT)
+        ok_button = Button(self, text="OK", command=self.show_result)
+        ok_button.pack(side=RIGHT)
 
-    def showResult(self):
-        msbox.showinfo(title= "RESULT", message= "DONE!")
+    def show_result(self):
+        msbox.showinfo(title="RESULT", message="DONE!")
+
 
 if __name__ == '__main__':
-
     root = Tk()
     root.geometry("500x300+400+200")
     app = Design(root)
